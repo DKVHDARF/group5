400c09f4816226b3f2099299cc189e52ccd6954d
fpf <yllamian@126.com>
2015-11-29 21:42:47 +0800

    项目完成

---
+++ pro/js/common.js
@@ -0,0 +1,22 @@
+/**
+ * Created by admin on 2015/11/29.
+ */
+
+$(".nav li").eq(0).click(function(){
+    location.href = "../fpf/index.html"
+});
+$(".nav li").eq(1).click(function(){
+    location.href = "../xsk/about.html"
+});
+$(".nav li").eq(2).click(function(){
+    location.href = "../hx/index.html"
+});
+$(".nav li").eq(3).click(function(){
+    location.href = "../yjf/project.html"
+});
+$(".nav li").eq(4).click(function(){
+    location.href = "../mzc/clients.html"
+});
+$(".nav li").eq(5).click(function(){
+    location.href = "../py/contacts.html"
+});
